8ab9049b7e7f02157d090ac28b8d10e8f037d263
Alex Gaston <alecgaston@gmail.com>
2024-01-17 16:37:21 +0100

    Entregas del modulo 1 y del modulo 2

---
+++ Modulo1 - HTML/README.md
@@ -0,0 +1,22 @@
+# LaboratorioHTML
+
+
+Tipografía
+La fuente utilizada es Montserrat de Google fonts 
+
+---
+
+Tamaños de fuente
+- Título: 32px
+- Tarjetas: 14px
+
+---
+
+Medidas y espaciados base:
+Ancho de pantalla en diseño: 1280px
+
+---
+
+Colores
+- Blanco: .... #ffffff
+- Negro: .... #212429
---
+++ Modulo2 - Tipos de datos y Operadores/src/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+import "./style.css";
+
+/*
+
+Tipos de datos y Operadores - Laboratorio:
+
+1. Quito los 18 euros de bebida al tiquet de la cena
+
+2. El resultado lo tendremos que dividir por todos los comensales para saber que hemos de pagar cada uno
+
+*/
+
+console.log("Cada comensal debe pagar: " + (120 - 18) / 7);
