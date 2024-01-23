dc57408d6fe8fb4871bed6120ce2dd06efb6b5d3
Alex Gaston <alecgaston@gmail.com>
2024-01-23 12:58:46 +0100

    SUbiendo entrega básica del Módulo 4

--- Modulo4 - Funciones/src/main.ts
+++ Modulo4 - Funciones/src/main.ts
@@ -1,47 +1,64 @@
 import "./style.css";
 
-/*
-function sumar() {
-  // Leer los valores de los inputs
-  const sumando1 = (document.getElementById("sumando1") as HTMLInputElement)
-    .value;
-  const sumando2 = (document.getElementById("sumando2") as HTMLInputElement)
-    .value;
-  // Sumar los dos números
-  const resultado = parseInt(sumando1) + parseInt(sumando2);
-  // Mostrar el resultado
-  const resultadoElement = document.getElementById("resultado");
+// Inicializamos el turnoInicial con valor a 1 y inicializamos donde se va a mostrar el turno
+let turnoInicial = 1;
 
-  if (resultadoElement !== null && resultadoElement !== undefined) {
-    resultadoElement.innerHTML = resultado.toString();
-  }
-}
-
-const botonSumar = document.getElementById("sumar");
-
-if (botonSumar !== null && botonSumar !== undefined) {
-  botonSumar.addEventListener("click", sumar);
-}
-*/
-
-// Inicializamos el turno
-const turnoInicial = 0;
-// funcion para incrmentar el turno
-function siguienteTurno(turnoInicial: number): number {
-  return turnoInicial + 1;
-}
-/*
-function anteriorTurno() {
-  return turno - 1;
-}
-
-function resetTurno() {
+// función para incrementar el turno
+const siguienteTurno = (): number => {
+  turnoInicial = turnoInicial + 1;
   return turnoInicial;
-}*/
+};
 
-const botonSiguiente = document.getElementById("siguiente");
-if (botonSiguiente !== null && botonSiguiente !== undefined) {
-  botonSiguiente.addEventListener("click", siguienteTurno);
+// función para decrementar el turno
+const anteriorTurno = (): number => {
+  turnoInicial = turnoInicial - 1;
+  return turnoInicial;
+};
+
+// función para resetear el contador de turno
+function resetTurno() {
+  const turnoActual = document.getElementById("numeroTurno");
+  const resetTurno = 0;
+  if (turnoActual !== null && turnoActual !== undefined) {
+    turnoActual.textContent = resetTurno.toString();
+  }
+  turnoInicial = 0;
+  return turnoInicial;
 }
+
+//Damos de alta el boton siguiente
+const botonSiguiente = document.getElementById("siguiente");
+
+//Creamos una funcion handler del BotonSiguiente que luego la utilizaremos en el boton siguiente
+const handlerBotonSiguiente = () => {
+  console.log(turnoInicial);
+  const turnoSiguiente = siguienteTurno();
+  const turnoActual = document.getElementById("numeroTurno");
+  if (turnoActual !== null && turnoActual !== undefined) {
+    turnoActual.textContent = turnoSiguiente.toString().padStart(2, "0");
+  }
+};
+
+if (botonSiguiente !== null && botonSiguiente !== undefined) {
+  botonSiguiente.addEventListener("click", () => {
+    handlerBotonSiguiente();
+  });
+}
+
+//Damos de alta el botón de anterior
 const botonAnterior = document.getElementById("anterior");
+//Creamos igual que en el boton siguiente un a función handler
+const handlerBotonAnterior = () => {
+  const turnoAnterior = anteriorTurno();
+  const turnoActual = document.getElementById("numeroTurno");
+  if (turnoActual !== null && turnoActual !== undefined) {
+    turnoActual.textContent = turnoAnterior.toString().padStart(2, "0");
+  }
+};
+
+botonAnterior?.addEventListener("click", () => {
+  handlerBotonAnterior();
+});
+//Damos de alta el botón de reset
 const botonReset = document.getElementById("reset");
+botonReset?.addEventListener("click", resetTurno);
