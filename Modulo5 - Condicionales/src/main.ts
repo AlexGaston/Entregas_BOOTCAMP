0f5890410f588ade5d0a103b4aef18a9e737712f
Alex Gaston <alecgaston@gmail.com>
2024-02-01 11:04:42 +0100

    Añadir mensaje en handleBotonQuePasaria

--- Modulo5 - Condicionales/src/main.ts
+++ Modulo5 - Condicionales/src/main.ts
@@ -287,6 +287,7 @@ const handleBotonQuePasaria = () => {
   handlePedirCarta();
   borrarBotonQuePasaria();
   deshabilitarBotonMePlanto(8);
+  pintarMensaje("¿Otra partida?");
 };
 
 const botonQuePasaria = document.getElementById("quepasaria");
