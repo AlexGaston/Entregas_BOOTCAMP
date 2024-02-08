9a042a5085266668eb980264acca5ed413d25aec
Alex Gaston <alecgaston@gmail.com>
2024-02-08 10:47:51 +0100

    Actualización de la entrega del Módulo 7

--- Modulo7 - Testing/src/motor.spec.ts
+++ Modulo7 - Testing/src/motor.spec.ts
@@ -1,4 +1,8 @@
-import { obtenerNumeroCarta, obtenerNumeroAleatorio } from "./motor";
+import {
+  obtenerNumeroCarta,
+  obtenerNumeroAleatorio,
+  obtenerPuntosDeLaCarta,
+} from "./motor";
 import { vi } from "vitest";
 
 describe("obtenerNumeroAleatorio", () => {
@@ -7,7 +11,7 @@ describe("obtenerNumeroAleatorio", () => {
     const numeroEsperado: number = 10;
     const spyOnMathRandom = vi
       .spyOn(global.Math, "random")
-      .mockReturnValue(0.999);
+      .mockReturnValue(0.99);
     //WHEN o Act
     const resultado = obtenerNumeroAleatorio();
     //THEN o Assert
@@ -42,6 +46,30 @@ describe("obtenerNumeroCarta", () => {
     const numeroAleatorio: number = 7;
     //WHEN o Act
     const resultado = obtenerNumeroCarta(numeroAleatorio);
+    //THEN o Assert
+    expect(resultado).toBe(7);
+  });
+});
+
+describe("obtenerPuntosDeLaCarta", () => {
+  it("Cuando la carta es una sota de copas el valor de la carta tiene que ser 0.5", () => {
+    //GIVEN o Arrange
+    const cartaSotaDeCopas: number = 10;
+
+    //WHEN o Act
+    const resultado = obtenerPuntosDeLaCarta(cartaSotaDeCopas);
+
+    //THEN o Assert
+    expect(resultado).toBe(0.5);
+  });
+
+  it("Cuando la carta es igual o menor de 7 el valor que nos tiene que devolver es el mismo valor que la carta recibida", () => {
+    //GIVEN o Arrange
+    const cartaSieteDeCopas: number = 7;
+
+    //WHEN o Act
+    const resultado = obtenerPuntosDeLaCarta(cartaSieteDeCopas);
+
     //THEN o Assert
     expect(resultado).toBe(7);
   });
