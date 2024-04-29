e528e577d984d4800193d1127711da4f756d8c1d
Alex Gaston <alecgaston@gmail.com>
2024-04-29 09:53:03 +0200

    Creación de la carpeta de la entrega 11

---
+++ Modulo 11 - Expresiones Regulares/src/model.ts
@@ -0,0 +1,158 @@
+const bancos = [
+  {
+    codigo: "2080",
+    nombreBanco: "Abanca Corporación Bancaria",
+  },
+  {
+    codigo: "0061",
+    nombreBanco: "Banca March",
+  },
+  {
+    codigo: "0188",
+    nombreBanco: "Banco Alcalá",
+  },
+  {
+    codigo: "0182",
+    nombreBanco: "Banco Bilbao Vizcaya Argentaria",
+  },
+  {
+    codigo: "0130",
+    nombreBanco: "Banco Caixa Geral",
+  },
+  {
+    codigo: "0234",
+    nombreBanco: "Banco Caminos",
+  },
+  {
+    codigo: "2105",
+    nombreBanco: "Banco Castilla-La Mancha",
+  },
+  {
+    codigo: "0240",
+    nombreBanco: "Banco de Crédito Social Cooperativo",
+  },
+  {
+    codigo: "0081",
+    nombreBanco: "Banco de Sabadell",
+  },
+  {
+    codigo: "0487",
+    nombreBanco: "Banco Mare Nostrum",
+  },
+  {
+    codigo: "0186",
+    nombreBanco: "Banco Mediolanum",
+  },
+  {
+    codigo: "0238",
+    nombreBanco: "Banco Pastor",
+  },
+  {
+    codigo: "0075",
+    nombreBanco: "Banco Popular Español",
+  },
+  {
+    codigo: "0049",
+    nombreBanco: "Banco Santander",
+  },
+  {
+    codigo: "3873",
+    nombreBanco: "Banco Santander Totta",
+  },
+  {
+    codigo: "2038",
+    nombreBanco: "Bankia",
+  },
+  {
+    codigo: "0128",
+    nombreBanco: "Bankinter",
+  },
+  {
+    codigo: "0138",
+    nombreBanco: "Bankoa",
+  },
+  {
+    codigo: "0152",
+    nombreBanco: "Barclays Bank PLC",
+  },
+  {
+    codigo: "3842",
+    nombreBanco: "BNP Paribas Paris",
+  },
+  {
+    codigo: "3025",
+    nombreBanco: "Caixa de Credit del Enginyers",
+  },
+  {
+    codigo: "2100",
+    nombreBanco: "Caixabank",
+  },
+  {
+    codigo: "2045",
+    nombreBanco: "Caja de Ahorros y Monte de Piedad de Ontinyent",
+  },
+  {
+    codigo: "3035",
+    nombreBanco: "Caja Laboral Popular CC",
+  },
+  {
+    codigo: "3081",
+    nombreBanco: "Caja Rural Castilla-La Mancha3058 Cajamar Caja Rural",
+  },
+  {
+    codigo: "2000",
+    nombreBanco: "Cecabank",
+  },
+  {
+    codigo: "1474",
+    nombreBanco: "Citibank Europe PLC",
+  },
+  {
+    codigo: "3821",
+    nombreBanco: "Commerzbank AG",
+  },
+  {
+    codigo: "3877",
+    nombreBanco: "Danske Bank A/S",
+  },
+  {
+    codigo: "0019",
+    nombreBanco: "Deutsche Bank SAE",
+  },
+  {
+    codigo: "0239",
+    nombreBanco: "EVO Banco",
+  },
+  {
+    codigo: "2085",
+    nombreBanco: "Ibercaja Banco",
+  },
+  {
+    codigo: "1465",
+    nombreBanco: "ING Bank NV",
+  },
+  {
+    codigo: "2095",
+    nombreBanco: "Kutxabank",
+  },
+  {
+    codigo: "2048",
+    nombreBanco: "Liberbank",
+  },
+  {
+    codigo: "0131",
+    nombreBanco: "Novo Banco",
+  },
+  {
+    codigo: "0073",
+    nombreBanco: "Open Bank",
+  },
+  {
+    codigo: "0108",
+    nombreBanco: "Société Générale",
+  },
+  {
+    codigo: "2103",
+    nombreBanco: "Unicaja Banco",
+  },
+];
