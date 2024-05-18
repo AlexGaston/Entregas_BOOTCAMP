d5b0443e2703ed23e2cfb93e94511467abfc5420
Alex Gaston <alecgaston@gmail.com>
2024-05-18 20:39:15 +0200

    Actualización Apartado B entreda RegEX

--- Modulo 11 - Expresiones Regulares/ApartadoB/src/main.ts
+++ Modulo 11 - Expresiones Regulares/ApartadoB/src/main.ts
@@ -15,27 +15,6 @@ const obtenerCodigoHTML = (): void => {
   extraerImagenesDelCodigo(codigoHTML);
 };
 
-/*const extraerImagenesDelCodigo = (codigoHTML: string) => {
-  const patron = /(http|https):\/\/.{1,}(webp|jpg|png|svg)"/gim;
-
-  const imagenesEncontradas = codigoHTML.match(patron);
-  console.log(imagenesEncontradas);
-
-  if (imagenesEncontradas !== null) {
-    const listado = document.querySelector("#imagenes-encontradas");
-    for (let i = 0; i < imagenesEncontradas.length; i++) {
-      const urlimagen = crearParrafo(imagenesEncontradas[i]);
-      if (
-        listado !== null &&
-        listado !== undefined &&
-        listado instanceof HTMLDivElement
-      ) {
-        listado.appendChild(urlimagen);
-      }
-    }
-  }
-};*/
-
 const botonExtraer = document.getElementById("extraer-imagenes");
 if (
   botonExtraer !== null &&
